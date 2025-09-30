7f2f31b869f81734f553ba22be92a76563506156
Bernardo <bernardo1075001@gmail.com>
2025-09-30 09:47:35 -0300

    Adicionando a cena FaseReparoPlaca
    Adcionei a fase reparo placa no bild profiles

--- reparo_placa/Assets/scripts/Jaize/TutorialFaseReparo.cs
+++ reparo_placa/Assets/scripts/Jaize/TutorialFaseReparo.cs
@@ -6,7 +6,7 @@ public class TutorialFaseReparo : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Iniciar()
     {
-        //SceneManager.LoadScene("fase1");
+        SceneManager.LoadScene("FaseReparoPlaca");
     }
 
     // Update is called once per frame
