30973e22725a481a817193062337e4f2182a66a8
Jaize-fs <jaizeferreira176@gmail.com>
2025-10-24 11:56:29 -0300

    Aliteração na fase de tutorial
    Realizei alterações na fase de tutorial da fase de reparo placa

---
+++ reparo_placa/Assets/scripts/Jaize/TextosTutorial.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TextosTutorial : MonoBehaviour
+{
+     [SerializeField] private GameObject Painel1;
+    [SerializeField] private GameObject Painel2;
+    [SerializeField] private GameObject Painel3;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void AtivarPainel2()
+    {
+        Painel1.SetActive(false);
+        Painel2.SetActive(true);
+        Painel3.SetActive(false);
+    }
+
+    public void AtivarPainel1()
+    {
+        Painel1.SetActive(true);
+        Painel2.SetActive(false);
+        Painel3.SetActive(false);
+    }
+    
+     public void AtivarPainel3()
+    {
+        Painel3.SetActive(true);
+        Painel1.SetActive(false);
+        Painel2.SetActive(false);
+    }
+}
